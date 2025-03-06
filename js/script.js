d9a3eab384a5ef4ca298f21d681e199107a04cda
Marco-Rocco <marcorocco28@icloud.com>
2025-03-06 15:01:19 +0100

    feat: create connection between html and js on <ul> and countown

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let countDown = document.getElementById('countdown')
+countDown.innerText = ('devasto seee')
+
+let numbersList = document.getElementById('numbers-list')
+numbersList.innerHTML = `
+<li>nonnt</li>
+<li>nonnt</li>
+<li>nonnt</li>
+<li>nonnt</li>
+<li>nonnt</li>`
