07bfdd413ba20560686ed5b40c138fd49170ea10
Marco-Rocco <marcorocco28@icloud.com>
2025-03-06 15:23:32 +0100

    feat: generate random number

--- js/script.js
+++ js/script.js
@@ -1,10 +1,38 @@
+//collegare il countdown
 let countDown = document.getElementById('countdown')
 countDown.innerText = ('devasto seee')
 
+//collegare la lista di numeri casuali
 let numbersList = document.getElementById('numbers-list')
+
+let randomNumber = function() {
+    return Math.floor(Math.random() * 99);
+}
+
 numbersList.innerHTML = `
-<li>nonnt</li>
-<li>nonnt</li>
-<li>nonnt</li>
-<li>nonnt</li>
-<li>nonnt</li>`
+<li>${randomNumber()}</li>
+<li>${randomNumber()}</li>
+<li>${randomNumber()}</li>
+<li>${randomNumber()}</li>
+<li>${randomNumber()}</li>`
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
